bf3d762b04b4ce66af9fcaba671d172f9497f818
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-05-08 12:41:51 +0100

    Add basic toggle to date picker

--- app/components/index.js
+++ app/components/index.js
@@ -1,3 +1,11 @@
+const theme = {
+  palette: {
+    primary: 'aquamarine',
+    highlight: 'deeppink',
+    magnolia: '#f7f7f7'
+  }
+}
+
 class DatePicker extends HTMLElement {
   constructor() {
     super();
@@ -6,18 +14,60 @@ class DatePicker extends HTMLElement {
 
     const wrapper = document.createElement("div");
     wrapper.setAttribute("class", "wrapper");
-    wrapper.innerHTML = "<p>Select due date</p>";
+    wrapper.innerHTML = `
+      ${this.selectedDate
+        ? `<p>${this.selectedDate}</p>`
+        : `<p class="select-date">Select due date</p>`
+      }
+      <div class="days">
+        <span id="1">1</span>
+        <span id="2">2</span>
+        <span id="3">3</span>
+        <span id="4">4</span>
+        <span id="5">5</span>
+        <span id="6">6</span>
+        <span id="7">7</span>
+      </div>
+    `
 
     const styles = document.createElement("style");
     styles.textContent = `
+      * {
+        box-sizing: border-box;
+        margin: 0;
+      }
       .wrapper {
         min-width: 200px;
         min-height: 50px;
-        background-color: red;
+        background-color: ${theme.palette.magnolia};
+        position: relative;
       }
-      p {
-        color: white;
-        margin: 0;
+      .select-date {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 50px;
+      }
+      .days {
+        display: none;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -25px;
+        height: 25px;
+        background-color: white;
+      }
+      .days.open {
+        display: flex;
+      }
+      .days span {
+        flex-grow: 1;
+        padding: 7px 0;
+        text-align: center;
+        color: ${theme.palette.primary};
+      }
+      .days span[selected] {
+        background-color: ${theme.palette.highlight};
       }
     `;
 
@@ -31,6 +81,16 @@ class DatePicker extends HTMLElement {
     return ["open"];
   }
 
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'open') {
+      const daySelector = this.shadowRoot.querySelector('.days')
+
+      if (newValue) return daySelector.classList.add('open')
+
+      daySelector.classList.remove('open')
+    }
+  }
+
   get open() {
     return this.hasAttribute("open");
   }
@@ -38,7 +98,7 @@ class DatePicker extends HTMLElement {
   setOpenState() {
     if (this.open) {
       return this.removeAttribute("open");
-    } else this.setAttribute("open", "");
+    } else this.setAttribute("open", true);
   }
 }
 
