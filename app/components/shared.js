06737404693b321d8b998c0d88836a01dadd741a
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-06-04 11:23:10 +0100

    Change to separate files and use css vars over js object

---
+++ app/components/shared.js
@@ -0,0 +1,21 @@
+const baseStyles = `
+  * {
+    --color-primary: lavender;
+    --color-highlight: deeppink;
+    --color-magnolia: #f7f7f7;
+    --color-dark: #0d0d0d;
+
+    box-sizing: border-box;
+    margin: 0;
+    color: var(--color-dark);
+  }
+
+  button {
+    background: none;
+    border: none;
+    outline: none;
+    padding: 0;
+  }
+`;
+
+export { baseStyles };
